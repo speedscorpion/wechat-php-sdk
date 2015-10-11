c74208be062ed779f32a9340ed6e5c134708342b
panboxiaosa <864633261@qq.com>
2015-10-11 13:54:58 +0800

    confit

--- example/server.php
+++ example/server.php
@@ -122,9 +122,9 @@
   }
 
   $wechat = new MyWechat(array(
-      'token' => 'Scorpion',
-      'aeskey' => 'EbNEy3iJ0UkMVTRCxVS66X1Hpc6b7HukAGAPyXyYk81',
-      'appid' => 'wx2ac64e7e153023af',
-      'debug' => true
+      'token' => $token,
+      'aeskey' => $encodingAesKey,
+      'appid' => $appId,
+      'debug' => $debugMode
   ));
   $wechat->run();
--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 
-  $encodingAesKey = "";
-  $token = "";
-  $appId = "";
+  $encodingAesKey = "EbNEy3iJ0UkMVTRCxVS66X1Hpc6b7HukAGAPyXyYk81";
+  $token = "Scorpion";
+  $appId = "wx2ac64e7e153023af";
   
   $debugMode = TRUE;
 
